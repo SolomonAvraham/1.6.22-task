65721e52c03fd0abcfea9ecff088da0a8b2d6025
SolomonAvraham <Selmon_a@hotmail.com>
2022-06-01 15:54:35 +0300

    updae

---
+++ main.js
@@ -0,0 +1,43 @@
+// 1. צרו אלמנט h1, שבלחיצה עליו מופעל alert , עם הודעה.
+
+function alertText() {
+  alert("STOP!");
+}
+
+// 2.
+
+function PrintPtoLog() {
+  console.log("SOLOMON");
+}
+
+// 3.
+
+function changeColor() {
+  var a = document.getElementById("three");
+  a.style.color = "red";
+}
+
+// 4.
+
+function alertH1() {
+  var e = document.getElementById("four");
+  e.style.fontSize = "10px";
+}
+
+// 5.
+function replaceText() {
+  var e = document.getElementById("six");
+  e.textContent = "Hello World!!!";
+}
+
+// 6.
+
+function PrintNameViaButton() {
+  var userInput = prompt("");
+  document.body.innerText = userInput;
+}
+
+
+// 7.
+
+
